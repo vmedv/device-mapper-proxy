a7ad15dfe31c37fdb3f1dc2c9e13bd8ec94e800d
ivan medvedev <vmedvedev1017@gmail.com>
2024-02-12 10:13:44 +0300

    collecting per-device stats

--- dmp.c
+++ dmp.c
@@ -5,7 +5,6 @@
  * This file is released under the GPL.
  */
 
-#include <asm-generic/errno-base.h>
 #include <linux/device-mapper.h>
 
 #include <linux/module.h>
@@ -19,6 +18,14 @@ typedef struct dmp_stat {
   size_t blk_size_sum;
 } dmp_stat;
 
+typedef struct dmp_dev {
+  struct dm_dev* dev;
+  dmp_stat read;
+  dmp_stat write;
+} dmp_dev;
+
+
+// overall stat
 dmp_stat dmp_read, dmp_write;
 
 /*
@@ -30,12 +37,22 @@ static int dmp_ctr(struct dm_target *ti, unsigned int argc, char **argv)
 		ti->error = "Invalid argument count";
 		return -EINVAL;
 	}
-  struct dm_dev* dt;
-  if (dm_get_device(ti, argv[0], dm_table_get_mode(ti->table), &dt)) {
+  struct dmp_dev* dt = kmalloc(sizeof(dmp_dev), GFP_KERNEL);
+
+  if (dt) {
+    ti->error = "dm-basic_target: Cannot allocate linear context";
+    return -ENOMEM;
+  }
+
+  if (dm_get_device(ti, argv[0], dm_table_get_mode(ti->table), &dt->dev)) {
     ti->error = "dm-basic_target: Device lookup failed";
     return -EINVAL;
   }
 
+  dt->read.blk_size_sum = 0;
+  dt->read.reqs = 0;
+  dt->write.blk_size_sum = 0;
+  dt->write.reqs = 0;
   ti->private = dt;
 
 	return 0;
@@ -46,22 +63,25 @@ static int dmp_ctr(struct dm_target *ti, unsigned int argc, char **argv)
  */
 static int dmp_map(struct dm_target *ti, struct bio *bio)
 {
-  struct dm_dev* dt = (struct dm_dev*) ti->private;
-  bio->bi_bdev = dt->bdev;
+  struct dmp_dev* dt = (dmp_dev*) ti->private;
+  bio->bi_bdev = dt->dev->bdev;
   switch (bio_op(bio)) {
     case REQ_OP_READ:
+      size_t sz = bio_sectors(bio) * SECTOR_SIZE;
+      dt->read.reqs++;
+      dt->read.blk_size_sum += sz;
       dmp_read.reqs++;
-      dmp_read.blk_size_sum += bio_sectors(bio) * SECTOR_SIZE;
+      dmp_read.blk_size_sum += sz;
       break;
     case REQ_OP_WRITE:
+      size_t sz = bio_sectors(bio) * SECTOR_SIZE;
+      dt->write.reqs++;
+      dt->write.blk_size_sum += sz;
       dmp_write.reqs++;
-      dmp_write.blk_size_sum += bio_sectors(bio) * SECTOR_SIZE;
-      break;
-    case REQ_OP_DISCARD:
-      /* writes get silently dropped */
+      dmp_write.blk_size_sum += sz;
       break;
     default:
-		return DM_MAPIO_KILL;
+      break;
 	}
 
 	bio_endio(bio);
@@ -72,7 +92,9 @@ static int dmp_map(struct dm_target *ti, struct bio *bio)
 
 static void dmp_dtr(struct dm_target *ti)
 {
-  dm_put_device(ti, ti->private);
+  struct dmp_dev* dt = (dmp_dev*) ti->private;
+  dm_put_device(ti, dt->dev);
+  kfree(dt);
 }
 
 static struct target_type dmp_target = {
@@ -85,5 +107,5 @@ static struct target_type dmp_target = {
 };
 module_dm(dmp);
 
-MODULE_DESCRIPTION(DM_NAME " ");
+MODULE_DESCRIPTION(DM_NAME " log simple stats");
 MODULE_LICENSE("GPL");
